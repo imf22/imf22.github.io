fc70821885355abd7ef2e6137e89bba4ced15c03
imf22 <92887508+imf22@users.noreply.github.com>
2023-12-11 16:48:52 -0500

    Test carousel completed images added

---
+++ ver3/slider.js
@@ -0,0 +1,23 @@
+// DOM elements
+const slider =
+document.querySelector('.image-slider');
+
+const arrLeft = 
+document.querySelector('.arrow-left');
+
+const arrRight = 
+document.querySelector('.arrow-right');
+
+const heading = 
+document.querySelector('.caption h1');
+
+const decription = 
+document.querySelector('.caption p');
+
+const imgs = [
+
+];
+
+const headings = [
+    
+];
