3294fe71f0dd3f461dad1653151b22e7a7a4eb0b
imf22 <92887508+imf22@users.noreply.github.com>
2023-12-12 16:11:46 -0500

    Carousel Functional - Fix button positioning - fix other styling

--- ver3/carousel.js
+++ ver3/carousel.js
@@ -1,15 +1,15 @@
-    // This carousel we functions by hiding the all image 
-    // DOM elements except the focused image.
-    // Images are dectected in the images div in the html
-    // new images can be added and changed there.
+// This carousel was modified to instead use backgroundImage to display
+// the focused image.
+// Images are managed through the html. Each img DOM src is stored 
+// and used to change the focused image. 
 document.querySelectorAll('.carousel').forEach(carousel => {
     const images = carousel.querySelector('.images');
     const prevButton = carousel.querySelector('.prev');
     const nextButton = carousel.querySelector('.next');
     
+    
     let imageIndex = 0;
     const totalImages = images.children.length;
-    const shiftY = Math.floor(100 / totalImages);
 
     const childElements = images.querySelectorAll('img');
     let childArray = [];
@@ -23,13 +23,11 @@ document.querySelectorAll('.carousel').forEach(carousel => {
     }
 
     prevButton.addEventListener('click', () => {
-      childArray[imageIndex].style.display ='none';
       imageIndex = (imageIndex - 1 + totalImages) % totalImages;
       updateImage();
     });
 
     nextButton.addEventListener('click', () => {
-      childArray[imageIndex].style.display ='none';
       imageIndex = (imageIndex + 1) % totalImages;
       updateImage();
     });
@@ -39,11 +37,7 @@ document.querySelectorAll('.carousel').forEach(carousel => {
         setTimeout(() => {
            images.classList.remove('image-fade') ;
         }, 550);
-        
-        childArray[imageIndex].style.display ='flex';
-      
+        carousel.style.backgroundImage = `url(${childArray[imageIndex].getAttribute('src')})`;
     }
 
-    updateImage();
-
-});
+  });
